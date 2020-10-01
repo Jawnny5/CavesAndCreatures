a28207a7900101bd5ce8cb8037cd98bde61263d7
jason1110 <Jason.easterly11@gmail.com>
2020-10-01 09:40:09 -0600

    move logic to Cli and add editing features

--- lib/models/avatar.rb
+++ lib/models/avatar.rb
@@ -23,22 +23,136 @@ class Avatar < ActiveRecord::Base
             )
 
         puts "great here is your new avatar"
-        pp new_avatar
-        puts "next we will build their starting equipment!"
-        Stat.get_avatar(new_avatar)
+        puts "Name: #{avatar_name}"
+        puts "Gender: #{avatar_gender}"
+        puts "Race: #{avatar_race}"
+        puts "Job: #{avatar_job}"
+        puts "Player: #{player}"
     end
 
-    # def stats
-    #     avatar_stats = CharacterStat.all.select {|statlist| statlist.stat.}
-    #     avatar_stats.map
-    # end
-
-    def create_character_stat (stat)
-        CharacterStat.create(stat_id: stat, avatar_id: self)
+    def change_name
+    prompt = TTY::Prompt.new
+        new_name = prompt.ask("What would you like to change #{self.name}'s name to?", default: "enter new name")
+        self.update(name: new_name)
+        "ok, #{self.name} it is!"
     end
 
-    def self.editor
-        puts "stuff to edit"
+
+    def level_up(avatar_job)
+        job_name = avatar_job
+        binding.pry
+        #case job_name
+        # when "Barbarian"
+        #  self.update(
+        #     strength: strength += 2
+        #     dexterity: dexterity += 1
+        #     constitution: constitution += 2
+        #     intelligence: intelligence += 0
+        #     wisdom: wisdom += 0
+        #     charisma: charisma += 1
+        #     hit_points: hit_points += 7
+        #     level: level += 1
+        #     )
+        # when "Bard"
+        #     t.integer "strength" += 0
+        #     t.integer "dexterity" += 2
+        #     t.integer "constitution" += 0
+        #     t.integer "intelligence" += 1
+        #     t.integer "wisdom" += 1
+        #     t.integer "charisma" += 3
+        #     t.integer "hit_points" += 5
+        #     t.integer "level" += 1
+        # when "Cleric"
+        #     t.integer "strength" += 1
+        #     t.integer "dexterity" += 0
+        #     t.integer "constitution" += 1
+        #     t.integer "intelligence" += 0
+        #     t.integer "wisdom" += 2
+        #     t.integer "charisma" += 1
+        #     t.integer "hit_points" += 5
+        #     t.integer "level" += 1
+        # when "Druid"
+        #     t.integer "strength" += 1
+        #     t.integer "dexterity" += 0
+        #     t.integer "constitution" += 1
+        #     t.integer "intelligence" += 0
+        #     t.integer "wisdom" += 2
+        #     t.integer "charisma" += 0
+        #     t.integer "hit_points" += 5
+        #     t.integer "level" += 1
+        # when "Fighter"
+        #     t.integer "strength" += 2
+        #     t.integer "dexterity" += 1
+        #     t.integer "constitution" += 2
+        #     t.integer "intelligence" += 0
+        #     t.integer "wisdom" += 0
+        #     t.integer "charisma" += 1
+        #     t.integer "hit_points" += 6
+        #     t.integer "level" += 1
+        # when "Monk"
+        #     t.integer "strength" += 1
+        #     t.integer "dexterity" += 2
+        #     t.integer "constitution" += 2
+        #     t.integer "intelligence" += 0
+        #     t.integer "wisdom" += 1
+        #     t.integer "charisma" += 0
+        #     t.integer "hit_points" += 6
+        #     t.integer "level" += 1
+        # when "Paladin"
+        #     t.integer "strength" += 2
+        #     t.integer "dexterity" += 0
+        #     t.integer "constitution" += 1
+        #     t.integer "intelligence" += 0
+        #     t.integer "wisdom" += 2
+        #     t.integer "charisma" += 1
+        #     t.integer "hit_points" += 6
+        #     t.integer "level" += 1
+        # when "Ranger"
+        #     t.integer "strength" += 1
+        #     t.integer "dexterity" += 2
+        #     t.integer "constitution" += 1
+        #     t.integer "intelligence" += 0
+        #     t.integer "wisdom" += 2
+        #     t.integer "charisma" += 0
+        #     t.integer "hit_points" += 6
+        #     t.integer "level" += 1
+        # when "Rogue"
+        #     t.integer "strength" += 0
+        #     t.integer "dexterity" += 2
+        #     t.integer "constitution" += 1
+        #     t.integer "intelligence" += 1
+        #     t.integer "wisdom" += 0
+        #     t.integer "charisma" += 2
+        #     t.integer "hit_points" += 5
+        #     t.integer "level" += 1
+        # when "Sorcerer"
+        #     t.integer "strength" += 0
+        #     t.integer "dexterity" += 0
+        #     t.integer "constitution" += 1
+        #     t.integer "intelligence" += 2
+        #     t.integer "wisdom" += 1
+        #     t.integer "charisma" += 2
+        #     t.integer "hit_points" += 4
+        #     t.integer "level" += 1
+        # when "Warlock"
+        #     t.integer "strength" += 0
+        #     t.integer "dexterity" += 0
+        #     t.integer "constitution" += 1
+        #     t.integer "intelligence" += 1
+        #     t.integer "wisdom" += 2
+        #     t.integer "charisma" += 2
+        #     t.integer "hit_points" += 5
+        #     t.integer "level" += 1
+        # when "Wizard"
+        #     t.integer "strength" += 0
+        #     t.integer "dexterity" += 0
+        #     t.integer "constitution" += 1
+        #     t.integer "intelligence" += 2
+        #     t.integer "wisdom" += 2
+        #     t.integer "charisma" += 0
+        #     t.integer "hit_points" += 4
+        #     t.integer "level" += 1
+        #end
     end
 
 
@@ -49,4 +163,3 @@ class Avatar < ActiveRecord::Base
 
 
 end
-
--- runner.rb
+++ runner.rb
@@ -1,6 +1,6 @@
 require_relative './config/environment.rb'
 
 app = Cli.new
-binding.pry
 app.welcome
 app.get_player
+app.main_menu
